59784ddca01311e1d099ef4041f6366cd3dfe0a6
Jensen-Jacob <jensenupwork@gmail.com>
2023-09-19 15:44:35 +0530

    Update footer to conditionally either tell the user to add items the list/show the packing status

--- src/App.js
+++ src/App.js
@@ -109,15 +109,21 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding items to your packing list!🚀</em>
+      </footer>
+    );
+  }
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
-  const packedPercent =
-    numItems === 0 ? 0 : Math.round((numPacked / numItems) * 100);
+  const packedPercent = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
       <em>
         {packedPercent === 100
-          ? "You've packed everything! You are ready to go ✈"
+          ? "You've packed everything! You are ready to go 🛫"
           : `💼You have ${numItems} items on your list and you already packed ${" "}
         ${numPacked} (${packedPercent}%)`}
       </em>
